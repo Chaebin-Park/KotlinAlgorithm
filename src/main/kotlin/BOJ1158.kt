7095fa7413a9c4d1920492b4e6a7f8bb2f1ecb8a
coqls <coqls0219@naver.com>
2021-10-19 17:24:01 +0900

    [ADD]BOJ1158

--- src/main/kotlin/BOJ1158.kt
+++ src/main/kotlin/BOJ1158.kt
@@ -1,24 +1,17 @@
 import java.util.*
 
 fun main() {
-    val N: Int
-    val K: Int
-
-    val input = readLine()
+    val (N, K) = readLine()!!.split(" ").map { it.toInt() }
     val queue: Queue<Int> = LinkedList()
-
-    N = input!!.split(" ")[0].toInt()
-    K = input.split(" ")[1].toInt()
+    val result = mutableListOf<Int>()
 
     for(i in 1..N) queue.add(i)
 
-    print("<")
     while(queue.isNotEmpty()) {
         for(i in 1 until K){
             queue.offer(queue.poll())
         }
-        print("${queue.poll()}")
-        if(queue.size != 0) print(", ")
+        result.add(queue.poll())
     }
-    print(">")
+    println("<".plus(result.joinToString(", ")).plus(">"))
 }
